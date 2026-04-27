6794c7cafefadda15b14d691256eb5a4902afb4c
MAIN-PC\Pc <dominykas.simpukas@gmail.com>
2026-04-27 21:26:04 +0300

    tested components and program running

--- SmartMirror/main/main.c
+++ SmartMirror/main/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+ #include "freertos/task.h"
 
 void app_main(void)
 {
+    printf("Main is running on %d core\n\r", xPortGetCoreID());
 
+    while(1)
+    {
+     printf("Hello world!\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));  // 1 second
+    }
 }
